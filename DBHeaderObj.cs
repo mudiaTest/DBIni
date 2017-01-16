9768adda79a278994a352a20c972762616618b77
mudia <mudia.pl@gmail.com>
2017-01-16 21:56:32 +0100

    Wlew inicjalizujący

---
+++ DBHeaderObj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ini2Flame
+{
+    public class DBHeaderObj
+    {
+        public string stFile;
+        public string stSection;
+        public bool blSelect;
+
+        public DBHeaderObj(string astFile,
+                           string astSection,
+                           bool ablSelect)
+        {
+            stFile = astFile;
+            stSection = astSection;
+            blSelect = ablSelect;
+        }
+    }
+}
